5e53c4ced443f4d8520a15a833972cdaa490763b
SneakySelderey <ershovsanek.centr@yandex.ru>
2023-07-13 14:31:11 +0300

    Some comments added

--- GameForm.cs
+++ GameForm.cs
@@ -12,7 +12,7 @@ namespace Minesweeper_WinForms
 {
     public partial class GameForm : Form
     {
-        public Form MainMenu { get; set; }
+        public Form MainMenu { get; set; }  // Here we store parent main menu form in order to show it again after this window is closed
         public System.Timers.Timer Timer { get; set; }
         public int Minutes { get; set; }
         public int Seconds { get; set; }
@@ -23,13 +23,13 @@ namespace Minesweeper_WinForms
             MainMenu = mainMenu;
         }
 
-        private void GameForm_FormClosed(object sender, FormClosedEventArgs e)
+        private void GameForm_FormClosed(object sender, FormClosedEventArgs e)  // Stop timer and show main menu when window is closed
         {
             Timer.Stop();
             MainMenu.Show();
         }
 
-        private void GameForm_Load(object sender, EventArgs e)
+        private void GameForm_Load(object sender, EventArgs e)  // Initialize timer when form loads
         {
             Timer = new();
             Timer.Interval = 1000;
@@ -37,7 +37,7 @@ namespace Minesweeper_WinForms
             Timer.Enabled = true;
         }
 
-        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)  // Update timer label text
         {
             Invoke(new Action(() =>
             {
