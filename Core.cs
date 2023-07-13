957f72c3610d78b899c2c7a115d991d4bea10098
SneakySelderey <ershovsanek.centr@yandex.ru>
2023-07-13 19:38:35 +0300

    Matrix generation reworked

--- Core.cs
+++ Core.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace Minesweeper_WinForms
 {
     internal class Core
     {
-        private bool[,] matrix; // this is minefield
+        private int[,] matrix; // this is minefield
         public int MatrixSize { get; set; }
         public int MinesNum { get; set; }
 
@@ -18,27 +19,64 @@ namespace Minesweeper_WinForms
             MinesNum = minesNum;
         }
 
-        public void GenerateMatrix()
+        internal void GenerateMatrix()
         {
             Random rnd = new();
-            matrix = new bool[MatrixSize, MatrixSize];
+            matrix = new int[MatrixSize, MatrixSize];
             for (int i = 0; i < MatrixSize; i++)
             {
                 for (int j = 0; j < MatrixSize; j++)
                 {
-                    matrix[i, j] = false; // generate empty minefield
+                    matrix[i, j] = 0; // generate empty minefield
                 }
             }
+
+            List<List<int>> availableCoords = new();
+            for (int i = 0; i < MatrixSize; i++)
+            {
+                for (int j = 0;j < MatrixSize; j++)
+                {
+                    availableCoords.Add(new List<int>() { i, j }); // generate a list of coords available to place mines
+                }
+            }
+
             int c = 0;
             while (c < MinesNum)
             {
-                List<int> mineCoord = new() { rnd.Next(0, MatrixSize), rnd.Next(0, MatrixSize) };
-                if (matrix[mineCoord[0], mineCoord[1]] == false)
+                int index = rnd.Next(0, availableCoords.Count); // pick a random set of coords from the list...
+                matrix[availableCoords[index][0], availableCoords[index][1]] = -1; // ...place a mine there
+                c++;
+                for (int i = availableCoords[index][0] - 1; i <= availableCoords[index][0] + 1; i++)
                 {
-                    matrix[mineCoord[0], mineCoord[1]] = true; // generate a given number of mines in random tiles of the field
-                    c++;
+                    for (int j = availableCoords[index][1] - 1; j <= availableCoords[index][1] + 1; j++)
+                    {
+                        try
+                        {
+                            if (matrix[i, j] != -1)
+                                matrix[i, j] += 1; // update a counter in all the tiles around the new mine
+                        }
+                        catch (IndexOutOfRangeException)
+                        {
+
+                        }
+                    }
                 }
+                availableCoords.RemoveAt(index); // make the coords unavailable
             }
+
+            //for (int i = 0; i < MatrixSize; i++)
+            //{
+            //    for (int j = 0; j < MatrixSize; j++)
+            //    {
+            //        Debug.Write($"{matrix[i, j]} ");
+            //    }
+            //    Debug.Write(Environment.NewLine);
+            //}
         }
+
+        //internal bool CheckTile(int row, int column, List<List<int>> result)
+        //{
+        //    if (matrix[row][column])
+        //}
     }
 }
